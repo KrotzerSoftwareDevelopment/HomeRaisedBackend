1edce26e0c5503a0e84b62bfc42410041c69e924
Dean Krotzer <krotzersoftwaredevelopment@gmail.com>
2020-12-06 01:51:51 -0800

    1.0.0v Server Production

---
+++ data.js
@@ -0,0 +1,118 @@
+export default {
+    background:[{
+        _id: '1',
+        image: '',
+        alt: '',
+    },
+    {
+        _id: '2',
+        image: '',
+        alt: '',
+    },
+    {
+        _id: '3',
+        image: '',
+        alt: '',
+    },
+    {
+        _id: '4',
+        image: '',
+        alt: '',
+    },{
+        _id: '5',
+        image: '',
+        alt: '',
+    }],
+
+    reservations: [{
+        _id: '1',
+        clientName: 'Sally K.',
+        clientLocation: 'Minnesota',
+        gender: 'female',
+        litter: 'next litter',
+        litterETA: '12/10/20',
+        reserved: 'yes'
+    },
+    {
+        _id: '2',
+        clientName: 'Kathy C.',
+        clientLocation: 'California',
+        gender: 'male or female',
+        litter: 'next litter',
+        litterETA: '12/10/20',
+        reserved: 'yes'
+    }],
+    puppies: [{
+        _id: '1',
+        puppyName: 'Emma',
+        dob: '05/10/20',
+        rank: 'Adopted', //Adopted or Available
+        color: 'Red-Brindle',
+        birthWeight: '',
+        birthTime: '',
+        momsName: 'Daphne',
+        dadsName: 'Cocoa',
+        icon: 'https://github.com/KrotzerSoftwareDevelopment/HomeRaisedCairns/blob/main/src/assets/Puppies/Origin%20Litter/Emma/emma.png?raw=true',
+        video: 'N/A',
+        gender: 'Female',
+        slideimage1: 'https://github.com/KrotzerSoftwareDevelopment/HomeRaisedCairns/blob/main/src/assets/Puppies/Origin%20Litter/Emma/emma.png?raw=true',
+        slideimage2: 'https://github.com/KrotzerSoftwareDevelopment/HomeRaisedCairns/blob/main/src/assets/Puppies/Origin%20Litter/Emma/emma2.png?raw=true',
+        slideimage3: 'https://github.com/KrotzerSoftwareDevelopment/HomeRaisedCairns/blob/main/src/assets/Puppies/Origin%20Litter/Emma/emma3.png?raw=true',
+        slideimage4: 'https://github.com/KrotzerSoftwareDevelopment/HomeRaisedCairns/blob/main/src/assets/Puppies/Origin%20Litter/Emma/emma4.png?raw=true',
+        discription: 'Adventurious, Playful, Joyful little girl puppy',
+        price: '2900',
+        priceincludes: '',
+        priceincludesTwo: '',
+        reg: 'Limited Akc Registration',
+        AdpotedFamily: 'Mike'
+    },
+    {
+        _id: '2',
+        puppyName: 'Leo',
+        dob: '05/10/20',
+        rank: 'Adopted', //Adopted or Available
+        color: 'Black-Brindle',
+        birthWeight: '',
+        birthTime: '',
+        momsName: '',
+        dadsName: '',
+        icon: 'https://github.com/KrotzerSoftwareDevelopment/HomeRaisedCairns/blob/main/src/assets/Puppies/Origin%20Litter/Leo/leo.png?raw=true',
+        video: 'N/A',
+        gender: 'Male',
+        slideimage1: 'https://github.com/KrotzerSoftwareDevelopment/HomeRaisedCairns/blob/main/src/assets/Puppies/Origin%20Litter/Leo/leo.png?raw=true',
+        slideimage2: 'https://github.com/KrotzerSoftwareDevelopment/HomeRaisedCairns/blob/main/src/assets/Puppies/Origin%20Litter/Leo/leo1.png?raw=true',
+        slideimage3: 'https://github.com/KrotzerSoftwareDevelopment/HomeRaisedCairns/blob/main/src/assets/Puppies/Origin%20Litter/Leo/leo2.png?raw=true',
+        slideimage4: 'https://github.com/KrotzerSoftwareDevelopment/HomeRaisedCairns/blob/main/src/assets/Puppies/Origin%20Litter/Leo/leo3.png?raw=true',
+        price: '2400',
+        priceincludes: '',
+        priceincludesTwo: '',
+        reg: 'Limited Akc Registration',
+        AdpotedFamily: 'Natalie'
+    },
+    {
+        _id: '3',
+        puppyName: 'Milo',
+        dob: '05/10/20',
+        rank: 'Adopted', //Adopted or Available
+        color: 'Wheaton',
+        birthWeight: '',
+        birthTime: '',
+        momsName: '',
+        dadsName: '',
+        icon: 'https://github.com/KrotzerSoftwareDevelopment/HomeRaisedCairns/blob/main/src/assets/Puppies/Origin%20Litter/Milo/milo.png?raw=true',
+        video: 'N/A',
+        gender: 'Male',
+        slideimage1: 'https://github.com/KrotzerSoftwareDevelopment/HomeRaisedCairns/blob/main/src/assets/Puppies/Origin%20Litter/Milo/milo2.png?raw=true',
+        slideimage2: 'https://github.com/KrotzerSoftwareDevelopment/HomeRaisedCairns/blob/main/src/assets/Puppies/Origin%20Litter/Milo/milo3.png?raw=true',
+        slideimage3: 'https://github.com/KrotzerSoftwareDevelopment/HomeRaisedCairns/blob/main/src/assets/Puppies/Origin%20Litter/Milo/milo4.png?raw=true',
+        slideimage4: '',
+        discription: 'Adventurious, Playful, Joyful little girl puppy',
+        price: '2400',
+        priceincludes: '',
+        priceinclude: '',
+        priceincludesTwo: '',
+        reg: 'Limited Akc Registration',
+        AdpotedFamily: 'Kaia'
+    }],
+  
+};
